bef19c69eacb53fd1378a148ab4bc3e8e967c2c5
Haley Rodzos <haley.rodzos@gmail.com>
2020-03-20 02:53:46 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -16,6 +16,8 @@ def hopper
       }
     }
 
+programmer_hash[:grace_hopper]
+
 end
 
 def alan_kay_is_known_for
@@ -36,6 +38,7 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
+programmer_hash[:alan_kay][:known_for]
 
 end
 
@@ -55,6 +58,8 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
+
+programmer_hash[:dennis_ritchie][:languages][0]
 
 end
 
@@ -82,6 +87,9 @@ def adding_matz
       }
     }
 
+
+programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby" , :languages => ["LISP", "C"]}
+programmer_hash
 end
 
 def changing_alan
@@ -104,7 +112,10 @@ def changing_alan
       }
     }
 
+
+
 end
+
 
 def adding_to_dennis
   # add "Assembly" to Dennis Ritchie's languages array
